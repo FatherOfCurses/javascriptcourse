7740d0a9c93c3803f5995299703d1d58998775d5
RexStardust <colin.principe@libertymutual.com>
2017-09-30 23:12:11 -0400

    HTML and Javascript on Display Evens both work

--- displayEvens.js
+++ displayEvens.js
@@ -1,38 +1,90 @@
-// Error handling functions
+function clearErrors() {
+    for (var loopCounter = 0;
+        loopCounter < document.forms["evenStepper"].elements.length;
+        loopCounter++) {
+        if (document.forms["evenStepper"].elements[loopCounter]
+            .parentElement.className.indexOf("has-") != -1) {
 
-function nonNumericEntry(startingNumber, endingNumber, stepValue) {
-    //TODO: write this evaluation
-}
-
-function stepValueIsNotPositive(stepValue) {
-    if(stepValue <=0) {
-        alert("Please enter a positive number in the Step field.")
+            document.forms["evenStepper"].elements[loopCounter]
+                .parentElement.className = "form-group";
+        }
     }
 }
 
-function endingNumberLower(startingNumber, endingNumber) {
-    if(endingNumber <= startingNumber) {
-        alert("Please enter an Ending Number greater in value than the Starting Number.")
-    }
-}
+function validateItems() {
+    clearErrors();
+    var startingNumber = Number(document.forms["evenStepper"]["startingNumber"].value);
+    var endingNumber = Number(document.forms["evenStepper"]["endingNumber"].value);
+    var stepValue = Number(document.forms["evenStepper"]["stepValue"].value);
 
-// write even number to array
-function writeNumberToArray(startingNumber, endingNumber, stepValue) {
+    if (startingNumber == "" || isNaN(startingNumber)) {
+        alert("Starting Number must be filled in with a number.");
+        document.forms["evenStepper"]["startingNumber"]
+            .parentElement.className = "form-group has-error";
+        document.forms["evenStepper"]["startingNumber"].focus();
+        return false;
+    }
+
+    if (endingNumber == "" || isNaN(endingNumber)) {
+        alert("Ending Number must be filled in with a number.");
+        document.forms["evenStepper"]["endingNumber"]
+            .parentElement.className = "form-group has-error";
+        document.forms["evenStepper"]["endingNumber"].focus();
+        return false;
+    }
+
+    if (stepValue == "" || isNaN(stepValue)) {
+        alert("Step Value must be filled in with a number.");
+        document.forms["evenStepper"]["stepValue"]
+            .parentElement.className = "form-group has-error";
+        document.forms["evenStepper"]["stepValue"].focus();
+        return false;
+    }
+
+    if (stepValue <= 0) {
+        alert("Please enter a positive number in the Step field.");
+        document.forms["evenStepper"]["stepValue"]
+            .parentElement.className = "form-group has-error";
+        document.forms["evenStepper"]["stepValue"].focus();
+        return false;
+    }
+
+    if (endingNumber < startingNumber) {
+        alert("Please enter an Ending Number greater in value than the Starting Number. Starting number " + startingNumber + " Ending number " + endingNumber);
+        document.forms["evenStepper"]["endingNumber"]
+            .parentElement.className = "form-group has-error";
+        document.forms["evenStepper"]["endingNumber"].focus();
+        return false;
+    }
+
+    // Populate array and provide output
     var numberSet = new Array();
-    if(startingNumber % 2 != 0) {
+    if (startingNumber % 2 != 0) {
         numberSet[0] = startingNumber + stepValue;
+    } else {
+        numberSet[0] = startingNumber;
     }
-    numberSet[0] = startingNumber;
-    var runningSum = startingNumber + stepValue;
+    var runningSum = numberSet[0] + stepValue;
+    var i = 1;
+
     do {
-        if(runningSum % 2 !=0) {
-            i++;
-        } else {
-            numberSet[i] = runningSum;
+        if (runningSum % 2 != 0) {
             runningSum += stepValue;
             i++;
-        } while (runningSum <= endingNumber);
+        } else {
+            numberSet.push(runningSum);
+            runningSum += stepValue;
+            i++;
+        }
     }
-    return numberSet;
-}
+    while (runningSum < endingNumber);
 
+    document.getElementById("leadInSentence").innerText = "Here are the even numbers between " + startingNumber + " and " + endingNumber + " by " + stepValue + "'s:";
+    var evenOutputText = "<ul>";
+    for (var outputLoop = 0; outputLoop < numberSet.length; outputLoop ++) {
+        evenOutputText += "<li>" + numberSet[outputLoop] + "</li>";
+    }
+    evenOutputText += "</ul>"
+    document.getElementById("evenOutput").innerHTML = evenOutputText
+    return false;
+}
